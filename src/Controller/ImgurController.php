3d600e1a9c9ae4cc319f76174485b7893f00714e
ging-dev <thanh1101dev@gmail.com>
2022-11-23 19:18:15 +0700

    update

--- src/Controller/ImgurController.php
+++ src/Controller/ImgurController.php
@@ -11,9 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-/**
- * @psalm-type ImgurResponse = array{data: array{id: string,title: null,description: null,datetime: int,type: string,animated: bool,width: int,height: int,size: int,views: int,bandwidth: int,vote: null,favorite: bool,nsfw: null,section: null,account_url: null,account_id: int,is_ad: bool,in_most_viral: bool,has_sound: bool,tags: list<string>,ad_type: int,ad_url: string,edited: string,in_gallery: bool,deletehash: string,name: string,link: string},success: bool,status: int}
- */
 #[Route('/imgur')]
 class ImgurController extends AbstractController
 {
@@ -30,20 +27,10 @@ class ImgurController extends AbstractController
     {
         $imgur = new Imgur();
 
-        $form = $this->createForm(ImgurType::class);
+        $form = $this->createForm(ImgurType::class, $imgur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var \Symfony\Component\HttpFoundation\File\UploadedFile */
-            $image = $form->get('image')->getData();
-
-            /** @var ImgurResponse */
-            $response = $imgurClient->request('POST', '/3/image', [
-                'body' => \fopen($image->getPathname(), 'r'),
-            ])->toArray();
-
-            $imgur->setLink($response['data']['link']);
-
             $imgurRepository->save($imgur, true);
 
             return $this->redirectToRoute('app_imgur_index', [], Response::HTTP_SEE_OTHER);

a7af51e36630bf2ee68d164dd139be73d9f3c2bd
ging-dev <thanh1101dev@gmail.com>
2022-12-02 21:11:21 +0700

    chore: only allow user

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[IsGranted('ROLE_USER')]
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
